a4bd1fd39bd9fe69dd4920c65e4cb4c73b9441ca
Tammy Swierczynski <101746947+cre8ivesolutions@users.noreply.github.com>
2022-08-13 10:15:08 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+//
+// DEPENDENCIES
+const express = require('express')
+// const mongoose = require('mongoose')
+// const languagesController = require('./controllers/language_controller')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true}, 
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
+
+// MIDDLEWARE
+// app.use(express.urlencoded({extended: true}))
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+// Languages: 
+// app.use('/languages', languagesController)
+
+// Random Languages: 
+// app.use('/languages/random', languagesController)
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log('Greetings! From port: ', PORT);
+})
